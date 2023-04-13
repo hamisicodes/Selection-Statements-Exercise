b6aa6b6529da878728a1f5f620435e4793600b3e
Hamisi Ngao <newhamisi@gmail.com>
2023-04-12 21:34:13 -0400

    complete ex 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random(); // initialize a random number generator
+            var favNumber = r.Next(1, 100) ; //generates random number >= 1 and < 100
+
+            Console.WriteLine("Guess a random number:");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(favNumber > userInput){
+                Console.WriteLine($"too low, the answer is {favNumber}");
+            }
+            else if(favNumber < userInput){
+                Console.WriteLine($"too high, the number is {favNumber}");
+            }
+            else{
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
